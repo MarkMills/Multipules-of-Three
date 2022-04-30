45dd143d9db523d146077ecff4b81c75f043ef74
MarkMills <98571190+MarkMills@users.noreply.github.com>
2022-04-29 19:01:07 -0600

    Create Multipules-of-Three.java

---
+++ Multipules-of-Three.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("Multiples of Three"); //print the title
+    
+    for(int i = 3; i <= 99; i = i + 3) { //for statement setting parameters 
+      System.out.println(i); //print multipules
+    }
+    
+  }
+}
